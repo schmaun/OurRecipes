768adb15d0f0623c859a8435591f7a5b1d597124
Jan Christiansen <jc@bhn-media.de>
2017-03-21 21:50:39 +0100

    fixed rotating

--- app/src/main/java/de/schmaun/ourrecipes/MainActivity.java
+++ app/src/main/java/de/schmaun/ourrecipes/MainActivity.java
@@ -53,7 +53,9 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        showLabelsListFragmentOnCreate();
+        if (savedInstanceState == null) {
+            showLabelsListFragmentOnCreate();
+        }
     }
 
     @Override
