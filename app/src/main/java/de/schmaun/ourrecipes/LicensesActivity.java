c9cf6ab465b0393b1803fa4e98897b75a1bfb32d
Jan Christiansen <jc@bhn-media.de>
2017-05-22 12:31:44 +0200

    added settings and licenses activity

---
+++ app/src/main/java/de/schmaun/ourrecipes/LicensesActivity.java
@@ -0,0 +1,13 @@
+package de.schmaun.ourrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LicensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_licenses);
+    }
+}
