5a6f7ba4fd9258da25173b475ce01f0e6151040e
Jan Christiansen <jc@bhn-media.de>
2017-05-22 22:24:26 +0200

    added licenses new launcher icon

--- app/src/main/java/de/schmaun/ourrecipes/LicensesActivity.java
+++ app/src/main/java/de/schmaun/ourrecipes/LicensesActivity.java
@@ -2,6 +2,9 @@ package de.schmaun.ourrecipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class LicensesActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class LicensesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_licenses);
+
+        TextView cookingIconText = (TextView) findViewById(R.id.licenses_cooking_icon);
+        cookingIconText.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView launcherIconText = (TextView) findViewById(R.id.licenses_launcher_icon);
+        launcherIconText.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+        TextView otherText = (TextView) findViewById(R.id.licenses_other);
+
+        String[] licenses = getResources().getStringArray(R.array.licenses_other);
+
+        for (String license : licenses) {
+            otherText.setText(otherText.getText() + license + "\n----------\n");
+        }
+
     }
 }
